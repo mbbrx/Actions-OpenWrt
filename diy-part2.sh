843d4b53af07aa484a3055c6b56a9959c2887af4
mbbrx <63870058+mbbrx@users.noreply.github.com>
2021-03-22 23:49:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/kenzok8/small package/small
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone https://github.com/kenzok8/small package/small
+#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+#git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone -b packages-19.07 https://github.com/liuran001/openwrt-packages
